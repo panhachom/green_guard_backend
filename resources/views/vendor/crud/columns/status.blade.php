0899e1ca4d713877690cbf0a3f6cf1fae0e67ccd
Nok sreyneang <noksreyneang607@gmail.com>
2024-03-23 22:16:00 +0700

    fixed

--- resources/views/vendor/crud/columns/status.blade.php
+++ resources/views/vendor/crud/columns/status.blade.php
@@ -7,12 +7,3 @@
 @else
 <span class="badge bg-warning">Unpublished</span>
 @endif
-
-@php
-    $status = $entry->status;
-@endphp
-
-@if ($status == 1)
-<span class="badge bg-success">Published</span>
-@else
-<span class="badge bg-warning">Unpublished</span>
